043d41950abcfa48bc827557907d7b14734fd654
Tomotaka Sakuma <ktmtmks@gmail.com>
2019-01-20 17:52:45 +0900

    sns icons with font-awesome 5

--- App.js
+++ App.js
@@ -3,5 +3,11 @@ import User from "./User.js";
 export default {
   name: "App",
   components: { User },
-  template: document.getElementById("app.html").innerHTML
+  template: document.getElementById("app.html").innerHTML,
+  data() {
+    return {
+      drawer: true,
+      items: [{ icon: "account_box", text: "account" }]
+    };
+  }
 };
